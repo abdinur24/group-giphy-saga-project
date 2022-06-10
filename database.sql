c4e6760f49bbe5cb6bfdabbe0eab83afc67f12ab
Abdishakur Nur <abdinur20000@gmail.com>
2022-06-10 10:50:57 -0500

    Set up Server side code

--- database.sql
+++ database.sql
@@ -12,3 +12,9 @@ CREATE TABLE "category" (
 -- Default categories. You may change them :)
 INSERT INTO "category" ("name")
 VALUES ('funny'), ('cohort'), ('cartoon'), ('nsfw'), ('meme');
+
+-- Favorite Table
+CREATE TABLE "favorites" (
+    "id" SERIAL PRIMARY KEY,
+	"category_id" INT REFERENCES "category"
+);
